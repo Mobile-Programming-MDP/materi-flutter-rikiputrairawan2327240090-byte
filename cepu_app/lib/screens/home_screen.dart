f527eb7988a51184f8dc518e553293063f9a4884
rikiputrairawan_2327240090 <rikiputrairawan_2327240090@mhs.mdp.ac.id>
2026-04-08 12:56:12 +0700

    new

--- cepu_app/lib/screens/home_screen.dart
+++ cepu_app/lib/screens/home_screen.dart
@@ -1,13 +1,53 @@
+import 'package:cepu_app/screens/sign_in_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  // Function Sign Out
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => const SignInScreen()),
+      (route) => false,
+    );
+  }
+
+  String generateAvatarUrl(String? fulltime) {
+    final formattedName = fulltime!.trim().replaceAll('','+');
+    return 'https://ui-avatars.com/api/?name=$formattedName&colour=FFFFFF&background=000000';
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home Screen")),
-      body: const Center(child: Text("You Have Been Signed In!")),
+      appBar: AppBar(
+        title: const Text("Home Screen"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () {
+              signOut();
+            },
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          "You Have Been Signed In!",
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
     );
   }
 }
